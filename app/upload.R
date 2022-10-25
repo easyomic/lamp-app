62cf5544886cfd58e7050edbd1bfe64d69b46f9b
$ LaBiOmicS <labiomics@bioinformatica.com.br>
2022-10-25 15:56:15 -0300

    Create upload.R

---
+++ app/upload.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(shinyFiles)
+
+### UI
+ui <- fluidPage(
+  shinyDirButton('directory_select', 'Select a directory', title='Select a directory'),
+  textOutput('directory_name')
+)
+
+### Server
+server <- function(input, output, session) {
+  volumes <- getVolumes()
+  shinyDirChoose(input, 'directory_select', roots=volumes, session=session)
+  dirname <- reactive({parseDirPath(volumes, input$directory_select)})
+
+  ## Observe input dir. changes
+  observe({
+    if(!is.null(dirname)){
+      print(dirname())
+      output$directory_name <- renderText(dirname())
+      ## Load files here
+      # csv <- read.csv(paste0(dirname(), '/filename1.csv'))
+      # rdata <- load(paste0(dirname(), '/filename2.Rdata'))
+      # etc.
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
